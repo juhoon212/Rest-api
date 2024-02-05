d8a986f905fc036c6c912eae92605693f913262f
juhoon212 <ws5501@naver.com>
2024-02-06 07:19:15 +0900

    Event

---
+++ src/test/java/com/example/demoinflearnrestapi/events/EventTest.java
@@ -0,0 +1,35 @@
+package com.example.demoinflearnrestapi.events;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class EventTest {
+
+
+    @Test
+    public void builder() {
+        Event event = Event.builder()
+                .name("Inflearn Spring REST API")
+                .description("REST API development with Spring")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean() {
+
+        String name = "Event";
+        String description = "Spring";
+
+        Event event = new Event();
+        event.setName(name);
+        event.setDescription(description);
+
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+    }
+
+}
